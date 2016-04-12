204c632cde15fb35f28a49bc70b84a4c14eef64b
Maciej Sulej <187809@edu.p.lodz.pl>
2016-04-12 18:01:18 +0200

    Test confirm dla zamowienia o utraconej waznosci

---
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -0,0 +1,29 @@
+package edu.iis.mto.time;
+
+import org.joda.time.DateTimeUtils;
+import org.joda.time.Duration;
+import org.junit.Before;
+import org.junit.Test;
+
+public class OrderTest {
+    final long ONE_DAY_IN_MILLIS = Duration.standardDays(1).getMillis();
+    final long TWO_DAYS_AGO = DateTimeUtils.currentTimeMillis() - ONE_DAY_IN_MILLIS * 2;
+
+    @Before
+    public void setUp() {
+        // Fix time if changed
+        DateTimeUtils.setCurrentMillisFixed(System.currentTimeMillis());
+    }
+
+    @Test (expected = OrderExpiredException.class)
+    public void confirmMethodShouldThrowOrderExpiredException() {
+        Order order = new Order();
+
+        order.submit();
+
+        DateTimeUtils.setCurrentMillisFixed(TWO_DAYS_AGO);
+
+        order.confirm();
+    }
+
+}
